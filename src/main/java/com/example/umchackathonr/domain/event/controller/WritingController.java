fcf7b90c490d2653b3e956c5bea254628f5c0f73
hyesoo kim <khwater1017@naver.com>
2024-01-02 22:24:49 +0900

    Docs: swagger 문서 설명 추가

--- src/main/java/com/example/umchackathonr/domain/event/controller/WritingController.java
+++ src/main/java/com/example/umchackathonr/domain/event/controller/WritingController.java
@@ -2,6 +2,8 @@ package com.example.umchackathonr.domain.event.controller;
 
 import com.example.umchackathonr.domain.event.service.WritingService;
 import com.example.umchackathonr.domain.event.dto.WritingRequestDto;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -12,10 +14,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/event/writing")
+@Tag(name = "chatGPT API", description = "chatGPT로 글 추천 API ")
 public class WritingController {
     private final WritingService writingService;
 
     @PostMapping("")
+    @Operation(summary = "글 추천", description = "목적과 성향기반으로 글을 추천합니다.")
     public ResponseEntity<?> createGhatGptWrite(@RequestBody WritingRequestDto writingRequestDto){
        return ResponseEntity.ok(writingService.getGhatGptWrite(writingRequestDto));
     }
