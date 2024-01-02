6e7be025ebbd7b950a8eac477b2ff3b44d56fd88
yerim216 <bing_01@naver.com>
2024-01-02 14:41:42 +0900

    Feat: 프로젝트 초기 세팅

---
+++ src/main/java/com/example/umchackathonr/UmcHackathonRApplication.java
@@ -0,0 +1,13 @@
+package com.example.umchackathonr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UmcHackathonRApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UmcHackathonRApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/umchackathonr/UmcHackathonRApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.umchackathonr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UmcHackathonRApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
